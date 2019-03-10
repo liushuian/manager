5be66f7eb2a80e6b545958e3f4fc55d671bd73f7
liushuian <1341030495.@qq.com>
2019-03-10 15:22:03 +0800

    完成登录页表单验证,布局,提交前数据验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,92 @@
 <template>
-  <div>
-      登录页
+  <div class="login">
+    <el-form
+      :model="formData"
+      status-icon
+      ref="ruleFrom2"
+      label-width="100px"
+      class="demo-ruleForm"
+      label-position="top"
+      :rules="rules"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model.trim="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model.trim="formData.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleFrom2')" id="btn-login">提交</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      formData: {
+        password: "",
+        username: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "change" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 16, message: "长度在 3 到 16 个字符", trigger: "change" }
+        ]
+      }
+    };
+  },
+   methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            // alert('submit!');
+          } else {
+            // console.log('error submit!!');
+            this.$message.error('请输入正确的用户名和密码');
+            return false;
+          }
+        });
+      },
+    }
+
+};
 </script>
 <style>
-    
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+body > div {
+  height: 100%;
+  width: 100%;
+}
+.login {
+  display: flex;
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: center;
+  background-color: #324152;
+  height: 100%;
+}
+.el-form {
+  background-color: #fff;
+  width: 623px;
+  height: 500px;
+  padding: 40px;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+#btn-login {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//导入路由
 import router from './lib/router'
+
+//导入饿了么Ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+ 
 
 new Vue({
   render: h => h(App),
